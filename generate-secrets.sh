15478ad8430f25c3a5425309bbc6f60db3cfdbb5
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-11-02 19:47:36 +0100

    Optimize script + add stub

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
 echo ""
-echo "Stubbing secrets from secrets.yaml"
+echo "Stubbing secrets from secrets.yaml..."
 echo ""
 
-sed 's/[ ].*//' < secrets.yaml > secrets.example.yaml
+{
+  :;
+  echo "# This file is an auto-generated stub of how the secrets file";
+  echo "# signature should look like. Do not modify this directly";
+  echo "# Signature: $(date +%x) - $(date | sha256sum)"
+  echo "";
+  sed 's/[ ].*//' < secrets.yaml
+  echo "";
+} >> secrets.example.yaml
