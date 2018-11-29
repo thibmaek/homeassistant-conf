85b0eb0ac1d8024e8070ce0b78a103456d6cf822
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-11-29 13:10:00 +0100

    Some more fixes for 404 on CSS file

--- www/custom-ui/animated-weather-card/animated-weather-card.js
+++ www/custom-ui/animated-weather-card/animated-weather-card.js
@@ -5,7 +5,7 @@ class DarkSkyWeatherCard extends HTMLElement {
       const link = document.createElement('link');
       link.type = 'text/css';
       link.rel = 'stylesheet';
-      link.href = '/local/custom_ui/animated-weather-card/animated-weather-card.css';
+      link.href = '/local/custom-ui/animated-weather-card/animated-weather-card.css';
       card.appendChild(link);
       this.content = document.createElement('div');
       this.content.className = 'card';
