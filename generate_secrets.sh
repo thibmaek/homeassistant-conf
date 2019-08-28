43bb23db21c040e55ba41a4b0ff4650bdc72962d
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-08-28 14:21:44 +0200

    Correctly parse secret values based on suffix

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -10,7 +10,29 @@ fi
 # mapfile -t secrets < <(grep -r '!secret' . --exclude-dir=.vscode --exclude=generate_secrets.sh | sed -ne 's/^.*!secret//p' | sort | uniq)
 secrets=($(grep -r '!secret' . --exclude-dir=.vscode --exclude-dir=esphome --exclude=generate_secrets.sh | sed -ne 's/^.*!secret//p' | sort | uniq))
 
+function getValue() {
+  if echo "$1" | grep -q "_port"; then
+    echo "8080"
+  elif echo "$1" | grep -q "_email"; then
+    echo "user@mail.com"
+  elif echo "$1" | grep -q "_host"; then
+    echo "0.0.0.0"
+  elif echo "$1" | grep -q "localhost"; then
+    echo "0.0.0.0"
+  elif echo "$1" | grep -q "_url"; then
+    echo "http://endpoint.com"
+  elif echo "$1" | grep -q "_token"; then
+    echo "e2394ce1-9898-426c-b897-dd351ded18b2"
+  elif echo "$1" | grep -q "_api_key"; then
+    echo "e2394ce1-9898-426c-b897-dd351ded18b2"
+  elif echo "$1" | grep -q "_secret"; then
+    echo "e2394ce1-9898-426c-b897-dd351ded18b2"
+  else
+    echo "any value"
+  fi
+}
+
 for secret in "${secrets[@]}"; do
   echo "Found secret: $secret"
-  echo "$secret: \"http://url.com\"" >> secrets.test.yaml
+  echo "$secret: \"$(getValue "$secret")\"" >> secrets.test.yaml
 done
